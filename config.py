fab8e620d1b22d6385c5151ecf0ea51a210f8e88
VietHTran <trantechenterprise@gmail.com>
2017-11-05 11:27:18 -0500

    Add Config

---
+++ config.py
@@ -0,0 +1,28 @@
+
+import os
+
+
+class Config(object):
+    """
+    Base class for application configuration details.
+    """
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TEMPLATES_AUTO_RELOAD = True
+
+
+class ProductionConfig(Config):
+    pass
+
+config = {
+    'dev': DevelopmentConfig,
+    'prod': ProductionConfig,
+    'default': ProductionConfig
+}
